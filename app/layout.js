3bd256bff32ca1fa713f87e2d43c633d7a32d241
Aaravsoni-01 <aaravsoni1381@gmail.com>
2026-06-29 10:10:40 +0530

    Fix: suppress hydration warning on HTML element caused by browser extensions

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${inter.variable} ${outfit.variable}`}>
+    <html lang="en" className={`${inter.variable} ${outfit.variable}`} suppressHydrationWarning>
       <body>
         <PlayerProvider>
           <div className="app-container">
